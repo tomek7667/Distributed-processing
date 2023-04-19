c29a6c87790af91559323602b2b61ba07e916a30
tomek7667 <38402753+tomek7667@users.noreply.github.com>
2023-04-19 12:34:19 +0000

    Lab6: Add more functionality and add security layers for the chat

--- lab6/client.py
+++ lab6/client.py
@@ -1,46 +1,47 @@
 import socket
 import threading
-
+import uuid
+from time import sleep
+DEFAULT_PORT = 3333
 
 HOST = '127.0.0.1'
-PORT = 65432
+PORT = input(f"Enter the port you want to join: ({DEFAULT_PORT})")
+NEXT_MESSAGE_TIMEOUT = 0.2
+if PORT == "":
+    PORT = f"{DEFAULT_PORT}"
+PORT = int(PORT)
 
 def receive_messages(sock, stop_event):
     while not stop_event.is_set():
-        data = sock.recv(1024)
-        if data:
-            print('\n| NEW MESSAGE: ', data.decode(), '\n')
+        data = sock.recv(1024).decode()
+        if data and data.startswith("SERVER:"):
+            data = data[7:]
+            print(f"Server: {data}")
+        elif data:
+            print(data)
         else:
             stop_event.set()
-
-    # Jeśli wątek został zakończony, to zamykamy socket
     sock.close() 
 
 def send_message(sock, user_name, stop_event):
     while not stop_event.is_set():
-        # Wprowadzenie wiadomości i odbiorcy
-        input_long = input("Enter recipient and message separated by ':' (or 'q' to exit):\n")
+        sleep(NEXT_MESSAGE_TIMEOUT)
+        input_long = input("")
 
         if input_long.lower() == "q":
             sock.sendall("q".encode())
             stop_event.set()  # Signal the threads to stop
             break
-        
-        try:
-            recipient, msg = input_long.split(":", 1)
-            recipient = recipient.strip()
-        except ValueError:
-            print("Invalid input format. Please enter the recipient's name and message separated by a colon (e.g. 'user111:hello')")
-            continue
-        
-        # Wysłanie wiadomości
-        sock.sendall(f"{recipient.strip()}: {msg.strip()}".encode())
+        sock.sendall(input_long.encode())
 
 def start_client():
-    # Wprowadzenie nazwy użytkownika
-    user_name = input("Enter your name: ")
+    # short uuid
+    generated_name = str(uuid.uuid4())[:5]
+    user_name = input(f"Enter your name: ({generated_name})").strip()
+    if user_name == "":
+        user_name = generated_name
+    
 
-    # Nawiązanie połączenia z serwerem
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
 
@@ -59,8 +60,7 @@ def start_client():
         # Oczekiwanie na zakończenie jednego z wątków
         receive_thread.join()
         send_thread.join()
-
-    print("Disconnected from the server.")
+    print("Disconnected\nThanks for using our chat!")
 
 if __name__ == "__main__":
     start_client()
--- lab6/server.py
+++ lab6/server.py
@@ -1,49 +1,96 @@
 import socket
 import threading
+import re
+import os
 
+PREFIX = "SERVER:"
+WELCOME_MESSAGE = f"{PREFIX}Welcome to the chat!\nIn order to disconnect type 'q'\nif you want to send a private message, type '/m <username> <message>'\nTo list current users type '/l'\n"
+USERNAME_REGEX = r"^[a-zA-Z0-9_-]+$"
+WRONG_USERNAME = f"{PREFIX}Please choose another name. (must match regex: {USERNAME_REGEX} and not already exist)"
 HOST = '127.0.0.1'
-PORT = 65432
+PORT = 3333
 
 users = {}
 
+def send_to_all(msg):
+    for user in users:
+        users[user].sendall(msg.encode())
+
+def send_to_all_except(msg, user_name):
+    for user in users:
+        if user != user_name:
+            users[user].sendall(msg.encode())
+
+def log(msg):
+    print(f"LOG: {msg}")
+
 def handle_client(conn, addr):
-    # Wprowadzenie nazwy użytkownika
     user_name = conn.recv(1024).decode()
+    if user_name in users or user_name == "SERVER" or not user_name or not re.match(USERNAME_REGEX, user_name):
+        conn.sendall(WRONG_USERNAME.encode())
+        conn.close()
+        return
 
-    # Dodanie użytkownika do listy użytkowników
     users[user_name] = conn
-
-    print(f"{user_name} has joined the chat.")
-
+    send_to_all_except(f"{PREFIX}{user_name} has joined the chat.", user_name)
+    conn.sendall(WELCOME_MESSAGE.encode())
+    log(f"{user_name} has joined the chat.")
     while True:
-        # Odbiór wiadomości
         data = conn.recv(1024)
         if not data or data.decode() == "q":
-            # Jeśli użytkownik wyszedł, usunięcie go z listy użytkowników i zamknięcie połączenia
             del users[user_name]
             conn.close()
-            print(f"{user_name} has left the chat.")
+            log(f"{user_name} has left the chat.")
+            send_to_all(f"{PREFIX}{user_name} has left the chat.")
             break
-
-        # Wysłanie wiadomości do wybranego użytkownika
         msg = data.decode()
-        recipient, msg = msg.split(":")
-        recipient = recipient.strip()
-        if recipient in users:
-            users[recipient].sendall(f"{user_name}: {msg}".encode())
+        if msg.startswith("/m"):
+            try:
+                recipient = msg.split(" ")[1]
+                if not re.match(USERNAME_REGEX, recipient):
+                    conn.sendall(f"{PREFIX}Wrong username format. Usage: \"/m <username> <message>\"".encode())
+                    continue
+                msg = " ".join(msg.split(" ")[2:])
+                if recipient in users:
+                    users[recipient].sendall(f"[M] {user_name}: {msg}".encode())
+                    log(f"{user_name} sent a private message to {recipient}: {msg}")
+                else:
+                    conn.sendall(f"{PREFIX}User \"{recipient}\" not found.".encode())
+            except:
+                conn.sendall(f"{PREFIX}Wrong format. Usage: \"/m <username> <message>\"".encode())
+        elif msg.startswith("/l"):
+            online = []
+            for user in users:
+                if user != user_name:
+                    online.append(user)
+                else:
+                    online.append(f"{user} (you)")
+            conn.sendall(f"{PREFIX}Users in the room: {', '.join(online)}".encode())
+        else:
+            send_to_all_except(f"{user_name}: {msg}", user_name)
 
-def start_server():
+def start_server(HOST, PORT):
     # Rozpoczęcie nasłuchiwania na porcie
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen()
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((HOST, PORT))
+            s.listen()
 
-        print(f"Server started. Listening on port {PORT}...")
+            print(f"Server started. Listening on port {PORT}...")
 
-        while True:
-            # Akceptowanie nowych połączeń i tworzenie nowego wątku dla każdego użytkownika
-            conn, addr = s.accept()
-            threading.Thread(target=handle_client, args=(conn, addr)).start()
+            while True:
+                conn, addr = s.accept()
+                threading.Thread(target=handle_client, args=(conn, addr)).start()
+    except KeyboardInterrupt:
+        send_to_all(f"{PREFIX}Server is shutting down...")
+        for user in users:
+            users[user].close()
+        s.close()
+        print("Server closed.")
+        exit(0)
+    except OSError:
+        PORT += 1
+        start_server(HOST, PORT)
 
 if __name__ == "__main__":
-    start_server()
+    start_server(HOST, PORT)
